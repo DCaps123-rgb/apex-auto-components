c9e058d918272a7c5ad33b86ee772e796d9678a1
unknown <roodtdirk@gmail.com>
2025-05-18 01:33:40 +0200

    Updated CSS, HTML, and JS files for Apex Auto Components website; removed unnecessary text document

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+
 function toggleMenu() {
     const navLinks = document.getElementById('navLinks');
     navLinks.classList.toggle('active');
+    const menuIcon = document.querySelector('.menu-icon');
+    if (navLinks.classList.contains('active')) {
+        menuIcon.innerHTML = '<i class="fas fa-times"></i>';
+    } else {
+        menuIcon.innerHTML = '<i class="fas fa-bars"></i>';
+    }
 }
 
-
+document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
